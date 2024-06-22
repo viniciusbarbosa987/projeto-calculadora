284b579cbd38323739c6916d0b96e20cfb21831b
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-06-21 21:37:11 -0300

    Já consigo guardar a expressão num array

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const tela = document.getElementById('tela')
 
+let identificadorDeExpressão = []
+
 
 function apagarCaractere(txt) {
     let resultado = ""
@@ -10,24 +12,34 @@ function apagarCaractere(txt) {
     return resultado
 }
 
-function exibirNaTela(valor) {
-    
-    if (tela.innerHTML == 0 && typeof(valor) === 'number') {
-        tela.innerHTML = null
-        tela.innerHTML += valor
-        // Evitar que o 0 apareça no primeiro algarismo do número exibido; A condição que usa o typeof serve para evitar que um operador(+,-,...) apareça no início da expressão
-    } else if(valor === 'clear'){
-        tela.innerHTML = 0
-        //zerar a expressão exibida
-    } else if(valor === 'apagarUltimoCaractere') {
-        tela.innerHTML = apagarCaractere(tela.innerHTML)
-        //apagar último caractere
-    } else {
-        tela.innerHTML += valor
+function funcionamentoCalculadora(valor) {
+    function exibirNaTela(valor) {
+        
+        if (tela.innerHTML == 0 && typeof(valor) === 'number') {
+            tela.innerHTML = null
+            tela.innerHTML += valor
+            // Evitar que o 0 apareça no primeiro algarismo do número exibido; A condição que usa o typeof serve para evitar que um operador(+,-,...) apareça no início da expressão
+        } else if(valor === 'clear'){
+            tela.innerHTML = 0
+            //zerar a expressão exibida
+        } else if(valor === 'apagarUltimoCaractere') {
+            tela.innerHTML = apagarCaractere(tela.innerHTML)
+            //apagar último caractere
+        } else {
+            tela.innerHTML += valor
+        }
+
     }
     
-    
-    
-    
+    function operação(valor) {
+        identificadorDeExpressão.push(valor)
+        
+        
+    }
 
+    exibirNaTela(valor)
+    operação(valor)
 }
+
+
+
